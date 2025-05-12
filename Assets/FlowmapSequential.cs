5365f8fa8fef7c98004c60fec49b6f0c894f1440
RoarlisF/KP31 <fangzhx@shanghaitech.edu.cn>
2025-05-11 17:18:14 -0700

    Encoding maps

---
+++ Assets/FlowmapSequential.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FlowmapSequential : MonoBehaviour
+{
+    [SerializeField] private Texture VtuFlowmaps;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
